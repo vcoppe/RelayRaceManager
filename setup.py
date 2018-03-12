f6b41bc8c5365f09dfb9e9fd0aa7f19ec92e4aea
vcoppe <vianney.coppe@student.uclouvain.be>
2018-03-12 13:26:44 +0100

    windows setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import cx_Freeze
+import sys
+import matplotlib
+import os
+
+os.environ['TCL_LIBRARY'] = r'C:\Users\igazi\AppData\Local\Programs\Python\Python36-32\tcl\tcl8.6'
+os.environ['TK_LIBRARY'] = r'C:\Users\igazi\AppData\Local\Programs\Python\Python36-32\tcl\tk8.6'
+
+base = None
+
+if sys.platform == 'win32':
+    base = "Win32GUI"
+
+executables = [cx_Freeze.Executable("gui.py", base=base, icon="Dragon.ico")]
+
+cx_Freeze.setup(
+    name = "Troupe des Dragons",
+    options = {"build_exe": {
+        "packages":["tkinter","matplotlib","numpy"],
+        "include_files":["Dragon.ico", r"tcl86t.dll", "tk86t.dll"]}},
+    version = "0.01",
+    description = "Chrono",
+    executables = executables
+    )
